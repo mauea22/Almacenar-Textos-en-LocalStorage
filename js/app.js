f1ac1b42ae807839b89d28952cfaefb5674acae8
mauea22 <mauroallena@gmail.com>
2022-05-27 14:53:33 -0300

    variables - agregarTweets- mostrarError

--- js/app.js
+++ js/app.js
@@ -17,7 +17,57 @@ function eventListener() {
 function agregarTweets(e) {
     e.preventDefault();
 
+    // Textarea donde el usuario escribe los tweets
+    const tweet = document.querySelector('#tweet').value;
+
+    //Validación....
+    if (tweet === '') {
+        mostrarError('Un mensaje NO puede enviarse vacío'); //llamamos a la función con su parámetro 
+        return; //este return evita que se ejecuten mas lineas
+    }
+    //crear el objeto de tweets
+    const tweetObj = {
+        /* Al no tener una base de datos en el proyecto (Date.now()) simula el id 
+        Date.now lo que hace es devolver en milisegundos el tiempo transcurrido desde 01/01/1970 
+        esto hace que cada texto que escribimos en el textarea y apretemos enviar 
+        nos agregue en el id: ese numero en milisegundos */
+        id: Date.now(), 
+        tweet // esto es igual a tweet: tweet
+    }
+    
+    //Añadir al arreglo de tweets el array actual mas los tweets que se van cargando 
+    tweets = [...tweets, tweetObj];
+
+    // Una vez agregado crear el HTML
+    crearHTML();
+
 
 }
 
+function mostrarError (error) {
+    const mensajeError = document.createElement('p'); //crea el párrafo (<p></p>)
+    mensajeError.textContent = error; //Agregael mensaje que le pasamos como parámetro al momento de llamar la función
+    mensajeError.classList.add('error'); //añade la clase .error (está en custom.css)
 
+    //insertarlo en el HTML
+    const insertarContenido = document.querySelector('#contenido');
+    insertarContenido.appendChild(mensajeError);
+
+    //quitar mensaje después de 2 segundos 
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 2000);
+}
+
+//Muestra los tweets en el HTML
+function crearHTML() {
+    if (tweets.length > 0) { //si el arreglo tiene algo
+        tweets.forEach( tweet => {
+            // crear el elemento
+            const li = document.createElement('li');
+
+            //añado el texto
+            li.textContent = tweet.tweet //? por cada elemento del array 
+        })
+    }
+}
